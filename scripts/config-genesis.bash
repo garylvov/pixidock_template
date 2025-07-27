449a8cc68872b7ea4a432c577b627f156fde474f
garylvov <gary.lvov@gmail.com>
2025-07-27 03:29:35 -0400

    goodnight fr

--- scripts/config-genesis.bash
+++ scripts/config-genesis.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-https://github.com/Genesis-Embodied-AI/Genesis/issues/1345
+# https://github.com/Genesis-Embodied-AI/Genesis/issues/1345
 
 # I think this means CPU rendering only
 export PYOPENGL_PLATFORM=osmesa
